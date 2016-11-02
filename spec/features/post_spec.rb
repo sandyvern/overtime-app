a86cfe543f4e87f7cb2d84d7559f0b9d2f6c1329
Sandy <SVern99@aol.com>
2016-11-02 17:43:59 +0000

    implemented creation process for posts and refactored

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -11,5 +11,23 @@ describe 'navigate' do
       visit posts_path
       expect(page).to have_content(/Posts/)
     end
+    
+    describe 'creation' do
+      before do
+        visit new_post_path
+      end
+      
+      it 'has a new form that can be created' do
+        expect(page.status_code).to eq(200)
+      end
+      
+      it 'can be created from new form page' do
+        fill_in "post[date]", with: Date.today
+        fill_in "post[rationale]", with: "Some rationale"
+        expect(page).to have_content("Some rationale")
+        
+        click_on "Save"
+      end
+    end
   end
 end
