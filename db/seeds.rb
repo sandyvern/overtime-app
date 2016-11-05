678495979eab2b7af1c2e4eedcddba331765ebcc
Sandy <SVern99@aol.com>
2016-11-05 20:06:53 +0000

    Implemented index view with posts, added table styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "John", last_name: "Snow")
+
+puts "1 user created"
 
 100.times do |post|
-    Post.create!(date: Date.today, rationale: "#{post} rationale content")
+    Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
