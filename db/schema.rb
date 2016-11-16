ae9def7e257538867a4b25ea5260a4238778ba03
Sandy <SVern99@aol.com>
2016-11-16 19:03:53 +0000

    added status to post, implemented enum for status in posts for workflow approval, updated readme status

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102212519) do
+ActiveRecord::Schema.define(version: 20161116185107) do
 
   create_table "posts", force: :cascade do |t|
     t.date     "date"
     t.text     "rationale"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
     t.integer  "user_id"
+    t.integer  "status",     default: 0
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
